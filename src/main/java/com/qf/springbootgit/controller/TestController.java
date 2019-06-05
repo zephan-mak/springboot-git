e6b1e669e992e53527c34857f06962aca95576b9
mzf <358866032@qq.com>
2019-06-05 17:51:12 +0800

    Initial commit

---
+++ src/main/java/com/qf/springbootgit/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.qf.springbootgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author maizifeng
+ * @Date 2019/6/5
+ */
+@Controller
+@RequestMapping("test")
+public class TestController {
+}
